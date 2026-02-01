6548cd2a7698cf2410c0f289628d85d83d6fa186
baddogpaul <paul@baddog.ie>
2026-02-01 17:42:27 +0000

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-async function loadPage() {
-  const res = await fetch("/api/page?slug=home");
-  const data = await res.json();
-
-  document.getElementById("title").innerText = data.title;
-  document.getElementById("content").innerHTML = data.body;
-}
-
-loadPage();
